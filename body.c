b3f03b357f5f593a99ba43ce1f0333d6960a7122
Daniel741312 <syc198@qq.com>
2020-11-25 08:45:14 +0800

    其他修改

--- body.c
+++ body.c
@@ -5,9 +5,17 @@
 #include<errno.h>
 #include<stdlib.h>
 #include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/mman.h>
 #include<dirent.h>
 
 void perr_exit(const char* str){
 	perror(str);
 	exit(1);
+}
+
+int main(int argc,char* argv[]){
+
+
+	return 0;
 }
