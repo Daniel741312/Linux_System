da49ec08f8c8f93c644837e3bc60ffdb5c57f01a
Daniel741312 <syc198@qq.com>
2020-11-27 19:31:17 +0800

    sigaction特性-信号捕捉函数期间屏蔽该信号

---
+++ 138_sigaction.c
@@ -0,0 +1,42 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/mman.h>
+#include<dirent.h>
+#include<signal.h>
+
+void perr_exit(const char* str){
+	perror(str);
+	exit(1);
+}
+
+void catch_signal(int signum){
+	if(signum==SIGINT)
+		printf("Catch you SIGINT:%d\n",signum);
+	sleep(10);
+	return;
+}
+
+int main(int argc,char* argv[]){
+
+	int ret=0;
+
+	struct sigaction act,oldact;
+	act.sa_handler=catch_signal;
+	sigemptyset(&act.sa_mask);
+	sigaddset(&act.sa_mask,SIGQUIT);
+	act.sa_flags=0;
+
+	ret=sigaction(SIGINT,&act,&oldact);
+	if(ret==-1)
+		perr_exit("sigaction error");
+
+	while(1);
+
+	return 0;
+}
