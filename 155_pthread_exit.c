620388051683659cccda0ffb7650c4b655fea1c7
Daniel741312 <syc198@qq.com>
2020-12-01 20:10:39 +0800

    pthread_exit函数的例子

---
+++ 155_pthread_exit.c
@@ -0,0 +1,41 @@
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<sys/stat.h>
+#include<sys/wait.h>
+#include<sys/mman.h>
+#include<dirent.h>
+#include<pthread.h>
+#include<signal.h>
+
+void perr_exit(const char* str){
+	perror(str);
+	exit(1);
+}
+
+void* tfn(void* arg){
+	int i=(int)arg;
+	if(i==2)
+		pthread_exit(NULL);
+
+	printf("I'm %dth thread,pid=%d,tid=%lu\n",i+1,getpid(),pthread_self());
+	sleep(i);
+	return NULL;
+}
+
+int main(int argc,char* argv[]){
+	int i=0;
+	int ret=0;
+	pthread_t tid=0;
+
+	for(i=0;i<5;++i){
+		ret=pthread_create(&tid,NULL,tfn,(void*)i);
+		if(ret!=0)
+			perr_exit("pthread_create error");
+	}
+	sleep(i);
+	return 0;
+}
